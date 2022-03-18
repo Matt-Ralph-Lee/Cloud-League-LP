dd929ddfe738eed5fe71fa9bc5da1bff6e1ff8f3
Yusima523 <matthewralphlee@gmail.com>
2022-03-18 22:15:38 +0900

    fly complete

--- src/Components/Fly.js
+++ src/Components/Fly.js
@@ -1,18 +1,38 @@
 import "./Fly.css";
 import React, { useState } from "react";
+import { useInView } from "react-intersection-observer";
 
-export const Fly = (props) => {
+export const Fly = () => {
+  const { ref, inView, entry } = useInView({
+    /* Optional options */
+    threshold: 0.8,
+  });
+
+  const [offset, setOffset] = useState(0);
+
+  window.addEventListener("scroll", parallax);
+
+  function parallax() {
+    if (ref) {
+      setOffset(window.scrollY);
+    }
+  }
+
+  const style = {
+    backgroundPosition: `0 ${offset / 3}px`,
+  };
+
   return (
     <>
-      <div className="flyPage">
-        <div className="fly">
+      <div className="flyPage" ref={ref}>
+        <div className="fly" style={style}>
           <span className="F">F</span>
           <span className="L">L</span>
           <span className="Y">Y</span>
           <span className="piriod">.</span>
-          {/* <div className="fly-circle"></div> */}
         </div>
       </div>
+      <div className="place-holder"></div>
     </>
   );
 };
