f172dd7223bd13a6156351a0e48bb22eddb46fbe
Yusima523 <matthewralphlee@gmail.com>
2022-03-21 19:26:47 +0900

    features fin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Fly } from "./Components/Organisms/Fly";
 import { Logo } from "./Components/Organisms/Logo";
 import { Concept } from "./Components/Organisms/Concept";
+import { Features } from "./Components/Organisms/Features";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Logo />
       <Fly />
       <Concept />
+      <Features />
     </div>
   );
 };
---
+++ src/Components/Organisms/Features/index.js
@@ -0,0 +1,96 @@
+import "./style.css";
+import React, { useEffect, useRef, useState } from "react";
+import CrossImage from "../../../Resources/image/Union.png";
+
+export const Features = () => {
+  const el = useRef(null);
+  const options = {
+    threshold: 0.8,
+  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(showElements, options);
+
+    observer.observe(el.current);
+    function showElements(entry) {
+      if (entry.isIntersecting) {
+        console.log(el.current);
+      }
+    }
+  }, []);
+  return (
+    <>
+      <div className="features-page">
+        <div className="features-title">FEATURES</div>
+        <div className="feature-one" ref={el}>
+          <div className="feature-number">01.</div>
+          <div className="feature-title">Hybrid Development</div>
+          <div className="feature-content">
+            <div className="feature-section">
+              <div>開発</div>
+              <img src={CrossImage} alt="" className="cross-image" />
+              <div>デザイン</div>
+              <img src={CrossImage} alt="" className="cross-image" />
+              <div>マーケ</div>
+            </div>
+            <div className="feature-explanation">
+              3つのバランスの取れた少人数チームで、
+            </div>
+            <div className="feature-explanation">
+              短期間でプロダクト開発のサイクルを回す。
+            </div>
+          </div>
+        </div>
+        <div className="feature-two">
+          <div className="feature-number">02.</div>
+          <div className="feature-title">Touch the Cutting Edge</div>
+          <div className="feature-content">
+            <div className="feature-section">
+              多様な分会で、最新技術を深掘り。
+            </div>
+            <div className="coffee-house">
+              <div className="coffee-house-title">
+                <div className="square-bullet"></div>
+                <div className="coffee-house-name">Crypto</div>
+              </div>
+              <div className="coffee-house-content">
+                WEB3.0関連企業でインターンをするメンバーが、
+              </div>
+              <div className="coffee-house-content">
+                クリプト・NFTの基礎知識を解説。
+              </div>
+            </div>
+            <div className="coffee-house">
+              <div className="coffee-house-title">
+                <div className="square-bullet"></div>
+                <div className="coffee-house-name">Security</div>
+              </div>
+              <div className="coffee-house-content">
+                詐欺やサイバー攻撃を防ぐために、
+              </div>
+              <div className="coffee-house-content">
+                情報セキュリティの脆弱性とその対策を学ぶ。
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="feature-three">
+          <div className="feature-number">03.</div>
+          <div className="feature-title">Competitive Melting Pot</div>
+          <div className="feature-content">
+            <div className="feature-section">
+              開発すれば開発する分だけ、
+              <br />
+              経験値が増える独自のシステムを導入。
+            </div>
+            <div className="feature-section">
+              日本各地に限らず海外に在住しているメンバーも多く、
+              <br />
+              コミュニティの視野の広さ、情報の多様性を維持。
+            </div>
+            <div className="feature-section"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
