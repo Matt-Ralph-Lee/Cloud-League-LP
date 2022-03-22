c152f314c447cab69362642623dfb6f3c6ee1e6d
Yusima523 <matthewralphlee@gmail.com>
2022-03-22 11:17:17 +0900

    customize cursor

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ import App from "./App.js";
 // ========================================
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const cursor = document.getElementById("cursor");
+const follower = document.getElementById("follower");
+
+document.addEventListener("mousemove", function (e) {
+  cursor.style.transform =
+    "translate(" + e.clientX + "px, " + e.clientY + "px)";
+});
+
+document.addEventListener("mousemove", function (e) {
+  follower.style.transform =
+    "translate(" + e.clientX + "px, " + e.clientY + "px)";
+});
